c436da1164098dc72852255f0440dc2dca5c6e84
Elladuke1 <skylimituc@gmail.com>
2023-04-21 15:56:01 -0700

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 
 	int d,p;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = '0'; d < '9' ; d++)
 	{
 
 	for (p = d + 1; p <= '9' ; p++)
@@ -24,7 +24,7 @@ int main(void)
 	putchar(p);
 	if (d == '8' && p == '9')
 	continue;
-	putchar(',' );
+	putchar(',');
 	putchar(' ');
 	}
 	}
