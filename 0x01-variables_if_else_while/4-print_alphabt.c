5b02f9a8123fc916d5196848c515ceb266ef0310
Elladuke1 <skylimituc@gmail.com>
2023-04-21 14:20:06 -0700

    ,alphabet 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,21 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 
 {
-	char low;
+
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-	
-	for (low = 'A'; low <= 'Z'; low++)
-	putchar(low);
+	{
+	if (low != e && low !=q)
+	putchar (low);
+	}
 	putchar('\n');
-
 	return (0);
 }
